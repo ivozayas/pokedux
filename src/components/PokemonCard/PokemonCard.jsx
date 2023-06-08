cd0d77d2dd43f3d1289a6a16fb205bbeee443e3c
Ivo Zayas <ivozayas2014@gmail.com>
2023-06-08 12:33:30 -0300

    staroutlined, antdicons

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -2,15 +2,19 @@ import { Card } from 'antd'
 import { CustomCardTitle } from './CustomTitle'
 import './index.css'
 import Meta from 'antd/es/card/Meta'
+import { StarOutlined } from '@ant-design/icons'
 
 function PokemonCard({ name, type }){
-    return <Card
+    return (
+        <Card
             className='pokemon-card'
             title={<CustomCardTitle name={name}/>}
             cover={<img src="" alt={name}/>}
+            extra={<StarOutlined/>}
         >
             <Meta className='pokemon-card-description' description={type}/>
         </Card>
+    )
 } 
 
 export { PokemonCard }
